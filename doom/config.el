d2071dd0acb3e7821a3edd5d45a91eb97e0b7a51
Arun <kumar.mvarun.arun@gmail.com>
2021-02-14 22:21:32 +0530

    Add default directory for emacs

--- doom/config.el
+++ doom/config.el
@@ -6,6 +6,7 @@
 (setq
  doom-font (font-spec :family "Source Code Pro for Powerline" :size 17 :weight 'Regular)
  doom-theme 'doom-gruvbox
+ default-directory "~"
  org-directory "~/org/"
  display-line-numbers-type 'relative)
 
