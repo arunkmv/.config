9854164752f7a6641ddca2dc3333ab1d8c713950
Arun <kumar.mvarun.arun@gmail.com>
2021-02-14 20:29:07 +0530

    Add doom emacs config

---
+++ doom/config.el
@@ -0,0 +1,14 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Maximized screen on start-up
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+(setq
+ doom-font (font-spec :family "Source Code Pro for Powerline" :size 17 :weight 'Regular)
+ doom-theme 'doom-gruvbox
+ org-directory "~/org/"
+ display-line-numbers-type 'relative)
+
+;; Use default treemacs theme instead of doom-atom theme
+(when (featurep! :ui treemacs)
+  (remove-hook 'doom-load-theme-hook #'doom-themes-treemacs-config))
