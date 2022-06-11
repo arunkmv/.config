63bf6e440ddc154917b334375c5d858a3490f342
arunkmv <arunkumarmv1997@gmail.com>
2022-06-11 10:14:21 +0530

    Change naming of custom symbols from ak/ to ak-

--- emacs/lisp/ak-scratch.el
+++ emacs/lisp/ak-scratch.el
@@ -1,16 +1,24 @@
-;;; lisp/scratch.el -*- lexical-binding: t; -*-
+;;; lisp/scratch.el --- A persistant scratch buffer utility -*- lexical-binding: t; -*-
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;; A scratch buffer utility with global and project specific scratch
+;; buffers that can persist across emacs sessions.
+;;
+;;; Code:
 
-(defvar ak/scratch-default-file "__default"
+(defvar ak-scratch-default-file "__default"
   "The default file name for a project-less scratch buffer.
 
-Will be saved in `ak/scratch-dir'.")
+Will be saved in `ak-scratch-dir'.")
 
-(defvar ak/etc-dir)
+(defvar ak-etc-dir)
 
-(defvar ak/scratch-dir (concat ak/etc-dir "scratch")
+(defvar ak-scratch-dir (concat ak-etc-dir "scratch")
   "Where to save persistent scratch buffers.")
 
-(defvar ak/scratch-initial-major-mode nil
+(defvar ak-scratch-initial-major-mode nil
   "What major mode to start fresh scratch buffers in.
 
 Scratch buffers preserve their last major mode, however, so this only affects
@@ -20,26 +28,26 @@ the first, fresh scratch buffer you create. This accepts:
   nil         Uses `fundamental-mode'
   MAJOR-MODE  Any major mode symbol")
 
-(defvar ak/scratch-buffers nil
+(defvar ak-scratch-buffers nil
   "A list of active scratch buffers.")
 
-(defvar ak/scratch-current-project nil
+(defvar ak-scratch-current-project nil
   "The name of the project associated with the current scratch buffer.")
-(put 'ak/scratch-current-project 'permanent-local t)
+(put 'ak-scratch-current-project 'permanent-local t)
 
-(defvar ak/scratch-buffer-hook ()
+(defvar ak-scratch-buffer-hook ()
   "The hooks to run after a scratch buffer is created.")
 
-(defun ak/load-persistent-scratch-buffer (project-name)
-  (setq-local ak/scratch-current-project
+(defun ak-load-persistent-scratch-buffer (project-name)
+  (setq-local ak-scratch-current-project
               (or project-name
-                  ak/scratch-default-file))
-  (when (not (file-exists-p ak/scratch-dir))
-    (make-directory ak/scratch-dir t))
+                  ak-scratch-default-file))
+  (when (not (file-exists-p ak-scratch-dir))
+    (make-directory ak-scratch-dir t))
   (let ((smart-scratch-file
-         (expand-file-name (concat ak/scratch-current-project ".el")
-                           ak/scratch-dir)))
-    (make-directory ak/scratch-dir t)
+         (expand-file-name (concat ak-scratch-current-project ".el")
+                           ak-scratch-dir)))
+    (make-directory ak-scratch-dir t)
     (when (file-readable-p smart-scratch-file)
       (message "Reading %s" smart-scratch-file)
       (cl-destructuring-bind (content point mode)
@@ -53,7 +61,7 @@ the first, fresh scratch buffer you create. This accepts:
         t))))
 
 ;;;###autoload
-(defun ak/scratch-buffer (&optional dont-restore-p mode directory project-name)
+(defun ak-scratch-buffer (&optional dont-restore-p mode directory project-name)
   "Return a scratchpad buffer in major MODE."
   (let* ((buffer-name (if project-name
                           (format "*scratch (%s)*" project-name)
@@ -66,43 +74,43 @@ the first, fresh scratch buffer you create. This accepts:
       (if dont-restore-p
           (erase-buffer)
         (unless buffer
-          (ak/load-persistent-scratch-buffer project-name)
+          (ak-load-persistent-scratch-buffer project-name)
           (when (and (eq major-mode 'fundamental-mode)
                      (functionp mode))
             (funcall mode))))
-      (cl-pushnew (current-buffer) ak/scratch-buffers)
-      (add-hook 'kill-buffer-hook #'ak/persist-scratch-buffer-h nil 'local)
-      (run-hooks 'ak/scratch-buffer-hook)
+      (cl-pushnew (current-buffer) ak-scratch-buffers)
+      (add-hook 'kill-buffer-hook #'ak-persist-scratch-buffer-h nil 'local)
+      (run-hooks 'ak-scratch-buffer-hook)
       (current-buffer))))
 
 ;;;###autoload
-(defun ak/persist-scratch-buffer-h ()
-  "Save the current buffer to `ak/scratch-dir'."
+(defun ak-persist-scratch-buffer-h ()
+  "Save the current buffer to `ak-scratch-dir'."
   (let ((content (buffer-substring-no-properties (point-min) (point-max)))
         (point (point))
         (mode major-mode))
     (with-temp-file
-        (expand-file-name (concat (or ak/scratch-current-project
-                                      ak/scratch-default-file)
+        (expand-file-name (concat (or ak-scratch-current-project
+                                      ak-scratch-default-file)
                                   ".el")
-                          ak/scratch-dir)
+                          ak-scratch-dir)
       (prin1 (list content
                    point
                    mode)
              (current-buffer)))))
 
 ;;;###autoload
-(defun ak/persist-scratch-buffers-h ()
-  "Save all scratch buffers to `ak/scratch-dir'."
-  (setq ak/scratch-buffers
-        (cl-delete-if-not #'buffer-live-p ak/scratch-buffers))
-  (dolist (buffer ak/scratch-buffers)
+(defun ak-persist-scratch-buffers-h ()
+  "Save all scratch buffers to `ak-scratch-dir'."
+  (setq ak-scratch-buffers
+        (cl-delete-if-not #'buffer-live-p ak-scratch-buffers))
+  (dolist (buffer ak-scratch-buffers)
     (with-current-buffer buffer
-      (ak/persist-scratch-buffer-h))))
+      (ak-persist-scratch-buffer-h))))
 
 ;;;###autoload
 (when (not noninteractive)
-  (add-hook 'kill-emacs-hook #'ak/persist-scratch-buffers-h))
+  (add-hook 'kill-emacs-hook #'ak-persist-scratch-buffers-h))
 
 
 ;;
@@ -110,7 +118,7 @@ the first, fresh scratch buffer you create. This accepts:
 
 (defvar projectile-enable-caching)
 ;;;###autoload
-(defun ak/open-scratch-buffer (&optional arg project-p same-window-p)
+(defun ak-open-scratch-buffer (&optional arg project-p same-window-p)
   "Pop up a persistent scratch buffer.
 
 If passed the prefix ARG, do not restore the last scratch buffer.
@@ -122,67 +130,67 @@ If PROJECT-P is non-nil, open a persistent scratch buffer associated with the
      (if same-window-p
 	 #'switch-to-buffer
        #'pop-to-buffer)
-     (ak/scratch-buffer
+     (ak-scratch-buffer
       arg
-      (cond ((eq ak/scratch-initial-major-mode t)
+      (cond ((eq ak-scratch-initial-major-mode t)
 	     (unless (or buffer-read-only
 			 (derived-mode-p 'special-mode)
 			 (string-match-p "^ ?\\*" (buffer-name)))
 	       major-mode))
-	    ((null ak/scratch-initial-major-mode)
+	    ((null ak-scratch-initial-major-mode)
 	     nil)
-	    ((symbolp ak/scratch-initial-major-mode)
-	     ak/scratch-initial-major-mode))
+	    ((symbolp ak-scratch-initial-major-mode)
+	     ak-scratch-initial-major-mode))
       default-directory
-      (when (and project-p (fboundp 'ak/project-name))
-		 (ak/project-name))))))
+      (when (and project-p (fboundp 'ak-project-name))
+		 (ak-project-name))))))
 
 ;;;###autoload
-(defun ak/switch-to-scratch-buffer (&optional arg project-p)
-  "Like `ak/open-scratch-buffer', but switches to it in the current window.
+(defun ak-switch-to-scratch-buffer (&optional arg project-p)
+  "Like `ak-open-scratch-buffer', but switches to it in the current window.
 
 If passed the prefix ARG, do not restore the last scratch buffer."
   (interactive "P")
-  (ak/open-scratch-buffer arg project-p 'same-window))
+  (ak-open-scratch-buffer arg project-p 'same-window))
 
 ;;;###autoload
-(defun ak/open-project-scratch-buffer (&optional arg same-window-p)
+(defun ak-open-project-scratch-buffer (&optional arg same-window-p)
   "Opens the (persistent) project scratch buffer in a popup.
 
 If passed the prefix ARG, do not restore the last scratch buffer."
   (interactive "P")
-  (ak/open-scratch-buffer arg 'project same-window-p))
+  (ak-open-scratch-buffer arg 'project same-window-p))
 
 ;;;###autoload
-(defun ak/switch-to-project-scratch-buffer (&optional arg)
-  "Like `ak/open-project-scratch-buffer', but switches to it in the current
+(defun ak-switch-to-project-scratch-buffer (&optional arg)
+  "Like `ak-open-project-scratch-buffer', but switches to it in the current
 window.
 
 If passed the prefix ARG, do not restore the last scratch buffer."
   (interactive "P")
-  (ak/open-project-scratch-buffer arg 'same-window))
+  (ak-open-project-scratch-buffer arg 'same-window))
 
 ;;;###autoload
-(defun ak/revert-scratch-buffer ()
+(defun ak-revert-scratch-buffer ()
   "Revert scratch buffer to last persistent state."
   (interactive)
   (unless (string-match-p "^\\*scratch" (buffer-name))
     (user-error "Not in a scratch buffer"))
-  (when (ak/load-persistent-scratch-buffer ak/scratch-current-project)
+  (when (ak-load-persistent-scratch-buffer ak-scratch-current-project)
     (message "Reloaded scratch buffer")))
 
 ;;;###autoload
-(defun ak/delete-persistent-scratch-file (&optional arg)
-  "Deletes a scratch buffer file in `ak/scratch-dir'.
+(defun ak-delete-persistent-scratch-file (&optional arg)
+  "Deletes a scratch buffer file in `ak-scratch-dir'.
 
 If prefix ARG, delete all persistent scratches."
   (interactive)
   (if arg
       (progn
-        (delete-directory ak/scratch-dir t)
-        (message "Cleared %S" (abbreviate-file-name ak/scratch-dir)))
-    (make-directory ak/scratch-dir t)
-    (let ((file (read-file-name "Delete scratch file > " ak/scratch-dir "scratch")))
+        (delete-directory ak-scratch-dir t)
+        (message "Cleared %S" (abbreviate-file-name ak-scratch-dir)))
+    (make-directory ak-scratch-dir t)
+    (let ((file (read-file-name "Delete scratch file > " ak-scratch-dir "scratch")))
       (if (not (file-exists-p file))
           (message "%S does not exist" (abbreviate-file-name file))
         (delete-file file)
