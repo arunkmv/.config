765c918001d896030b7402ed7498039b9e150e74
arunkmv <arunkumarmv1997@gmail.com>
2022-08-19 23:06:53 +0530

    Add chemacs config

---
+++ chemacs/profiles.el
@@ -0,0 +1,4 @@
+(("doom" . ((user-emacs-directory . "~/.emacs.doom")
+	       (env . (("DOOMDIR" . "~/.config/doom")))))
+ ("default" . ((user-emacs-directory . "~/.config/emacs")))
+ ("vanilla" . ((user-emacs-directory . "~/.config/vanilla-emacs"))))
