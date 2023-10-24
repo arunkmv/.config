d170fd72e8b2b22230e8002c7e6f9cd571a73d3e
arunkmv <arunkumarmv1997@gmail.com>
2023-10-24 12:14:31 +0200

    Add gcmh mode

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,6 +1,11 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 ;;
 
+;; PERF: Garbage collection is a big contributor to startup times. This fends it
+;;   off, but will be reset later by `gcmh-mode'. Not resetting it later will
+;;   cause stuttering/freezes.
+(setq gc-cons-threshold most-positive-fixnum)
+
 ;; In Emacs 27+, package initialization occurs before `user-init-file' is
 ;; loaded, but after `early-init-file'. Straight handles package initialization, so
 ;; we must prevent Emacs from doing it early!
